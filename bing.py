017ed2d803a6c1aa57987b3eef70af5804f3f458
Josh Rozner <jsrozner@gmail.com>
2013-09-29 15:54:00 -0500

    fixed small args bug

--- bing.py
+++ bing.py
@@ -59,7 +59,7 @@ phrases = {
    5:'I want to'}          # <adverb> + <verb>
 
 def rand_sleep():
-  if !norandomsleep:
+  if !args.norandomsleep:
     time.sleep(kMaxSleep * random.random())
 
 def generate_search():
